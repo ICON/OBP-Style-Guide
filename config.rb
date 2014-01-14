e350fb339d26de0e78ba5c25897ea3c9f2c09ebd
ICON <connie@offbaseproductions.com>
2014-01-14 11:48:46 -0800

    styleguide CSS, tooltips, sidenav, layout content

--- config.rb
+++ config.rb
@@ -4,9 +4,9 @@ add_import_path "bower_components/foundation/scss"
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "scss"
+sass_dir = "stylesheets/scss"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
--- js/app.js
+++ js/app.js
@@ -4,31 +4,28 @@ $(document).foundation();
 
 var app = angular.module('styleGuideApp', ['ngAnimate', 'ngRoute']);
 
-
 app.config( function ($routeProvider) {
-
-	//================================================
-	// Define all the routes
-	//================================================
-
-	$routeProvider
-		.when('/404',
-		{
-			title: 'Oh Oh!',
-			templateUrl: 'partials/not-found/404-tmpl.html',
-			controller: '404Ctrl'
-		})
-		.when('/',
-		{
-			title: 'Home',
-			templateUrl: 'partials/layout-tmpl.html',
-			controller: routeCtrl
-		})
-		.when('/:slug',
-		{
-			templateUrl: 'partials/url-router.html',
-			controller: routeCtrl
-		});
+//================================================
+// Define all the routes
+//================================================
+$routeProvider
+	.when('/404',
+	{
+		title: 'Oh Oh!',
+		templateUrl: 'partials/not-found/404-tmpl.html',
+		controller: '404Ctrl'
+	})
+	.when('/',
+	{
+		title: 'Home',
+		templateUrl: 'partials/layout-tmpl.html',
+		controller: routeCtrl
+	})
+	.when('/:slug',
+	{
+		templateUrl: 'partials/url-router.html',
+		controller: routeCtrl
+	});
 });
 
 app.controller('navCtrl', function($scope, $location) {
@@ -39,11 +36,15 @@ app.controller('navCtrl', function($scope, $location) {
 			$location.hash(hash);
 		}
 	};
-
 });
 
 var routeCtrl = function($scope, $routeParams) {
 	console.log('ROUTE PARAMS: ', $routeParams.slug);
     $scope.templateUrl = 'partials/'+$routeParams.slug+'-tmpl.html';
+
+  $scope.$on('$viewContentLoaded', function() {
+  	$(document).foundation();
+  });
 };
+
 
