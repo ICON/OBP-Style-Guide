3ac7a9cce7450084291e2eac6105ef710c9dd46a
slb350 <brandon.steve@gmail.com>
2014-05-21 14:58:58 -0700

    added deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :branch, 'master'
 server 'cva-styles.digiman.us', user: 'cpt', roles: %w{web app}
 
  set :ssh_options, {
-   keys: %w(~/.ssh/staging.pem),
+ #  keys: %w(~/.ssh/staging.pem),
    forward_agent: true,
    auth_methods: %w(publickey password)
  }
